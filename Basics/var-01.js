29bb5032827d4715da5aaae8444a772899504bb0
Sumit Chaturvedi <122745380+sumitchaturvedi15@users.noreply.github.com>
2024-10-06 17:10:40 +0000

    container config done

---
+++ Basics/var-01.js
@@ -0,0 +1,9 @@
+const accountId="123456"
+let Email="sumitchaturvedilucky2002@gmail.com"
+var password="jaihind"
+city="Indore"
+
+console.log(accountId)
+console.table([accountId, Email, password, city])
+
+//Prefer to use let instead of using var to store variables
