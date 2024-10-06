8b64486a7b9b9473e6892fa69c520967fbc07bb2
Sumit Chaturvedi <122745380+sumitchaturvedi15@users.noreply.github.com>
2024-10-06 18:16:11 +0000

    Conversion pushed

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,9 @@
+let score="33"
+let convertedNum=Number(score)
+console.log(typeof convertedNum)
+console.log(convertedNum)
+
+let numberBe=52
+let convertString= String(numberBe)
+console.log(typeof convertString)
+console.log(convertString)
