d2f59e0ca6d61244bfd283cbca16cc4edd02a257
Sumit Chaturvedi <122745380+sumitchaturvedi15@users.noreply.github.com>
2024-10-12 18:47:42 +0000

    Maths and number IMP's

---
+++ Basics/number_maths.js
@@ -0,0 +1,19 @@
+// const score = 100
+// const account = new Number(100)
+// console.log(account)
+// console.log(account.toFixed(2))
+// const number=100000000
+// console.log(number.toLocaleString('en-IN'))
+
+// maths
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10));
+// console.log((Math.random()*10)+1);
+//imp*****************************************imp
+const min=1
+const max=6
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
