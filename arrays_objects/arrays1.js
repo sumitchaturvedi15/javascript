d2fe28d1f749402e89e48c8738542b958c8783ab
Sumit Chaturvedi <122745380+sumitchaturvedi15@users.noreply.github.com>
2025-02-19 17:32:02 +0000

    Array added

---
+++ arrays_objects/arrays1.js
@@ -0,0 +1,21 @@
+// const marvel=['thor', 'ironman']
+// const dc=['superman', 'flash']
+// // marvel.push(dc)
+// // console.log(marvel) //[ 'thor', 'ironman', [ 'superman', 'flash' ] ]
+// //accessing inner elements
+// // console.log(marvel[2][0]);//superman
+// const arr=marvel.concat(dc) //[ 'thor', 'ironman', 'superman', 'flash' ]
+// console.log(arr);
+// const arr1=[...marvel, ...dc] //[ 'thor', 'ironman', 'superman', 'flash' ]
+// console.log(arr1)
+
+// const arr=[1,2,[2,4,[3,1]],[1,9]]
+// const flatArr=arr.flat(Infinity)
+// console.log(flatArr); //1, 2, 2, 4, 3, 1, 1, 9
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1, score2, score3));
+
